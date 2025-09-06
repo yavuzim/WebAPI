66f3f3ef0bd41a6811878109e3d0476a0cba0fbf
yavuzim <kyavuzimer@gmail.com>
2025-09-06 22:19:41 +0300

    Contact DTO implemented

---
+++ WebAPI/Controllers/ContactsController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Context;
+using WebAPI.Dtos.ContactDtos;
+using WebAPI.Entities;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly APIContext context;
+        public ContactsController(APIContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult ContactList()
+        {
+            var values = context.Contacts.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("GetContact")]
+        public IActionResult GetContact(int id)
+        {
+            var value = context.Contacts.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateContact(CreateContactDto createContactDto)
+        {
+            Contact contact = new Contact()
+            {
+                MapLocation = createContactDto.MapLocation,
+                Address = createContactDto.Address,
+                Phone = createContactDto.Phone,
+                Email = createContactDto.Email,
+                OpenHours = createContactDto.OpenHours
+            };
+            context.Contacts.Add(contact);
+            context.SaveChanges();
+            return Ok("Contact added successfully.");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateContact(UpdateContactDto updateContactDto)
+        {
+            Contact contact = new Contact()
+            {
+                ContactId = updateContactDto.ContactId,
+                MapLocation = updateContactDto.MapLocation,
+                Address = updateContactDto.Address,
+                Phone = updateContactDto.Phone,
+                Email = updateContactDto.Email,
+                OpenHours = updateContactDto.OpenHours
+            };
+            context.Contacts.Update(contact);
+            context.SaveChanges();
+            return Ok("Contact updated successfully.");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteContact(int id)
+        {
+            var value = context.Contacts.Find(id);
+            context.Contacts.Remove(value);
+            context.SaveChanges();
+            return Ok("Contact deleted successfully.");
+        }
+    }
+}
