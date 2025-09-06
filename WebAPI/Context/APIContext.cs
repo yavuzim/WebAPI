3ce808770952fc74613a1c2eac25d881f45a7ca9
yavuzim <kyavuzimer@gmail.com>
2025-09-06 20:28:30 +0300

    Entites and migrations completed

---
+++ WebAPI/Context/APIContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Entities;
+
+namespace WebAPI.Context
+{
+    public class APIContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("connection.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        public DbSet<Category> Categories {  get; set; }
+        public DbSet<Chef> Chefs {  get; set; }
+        public DbSet<Contact> Contacts {  get; set; }
+        public DbSet<Feature> Features {  get; set; }
+        public DbSet<Image> Images {  get; set; }
+        public DbSet<Message> Messages {  get; set; }
+        public DbSet<Product> Products {  get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Service> Services {  get; set; }
+        public DbSet<Testimonial> Testimonials {  get; set; }
+    }
+}
---
+++ WebAPI/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace WebAPI.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
+}
